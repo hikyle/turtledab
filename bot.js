f3b2c8e58afd688a65e132d9f1909fb98e375b2e
Kyle <hikyle@users.noreply.github.com>
2020-05-08 03:14:12 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('ready');
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'text-based-memage');
+    
+    if (!channel) {
+        console.log('channel not found');
+        return;
+    }
+
+    channel.send('https://tenor.com/view/forrest-gump-forrest-gump-wave-hello-gif-5576725 @${member}');
+});
+
+client.on('guildMemberRemove', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'text-based-memage');
+
+    if (!channel) {
+        console.log('channel not found');
+        return;
+    }
+
+    channel.send('${member} has left https://tenor.com/view/peace-gif-9727828');
+});
+
+client.login(process.env.BOT_TOKEN);
