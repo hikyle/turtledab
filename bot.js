b287ee11917cf8cc854f212261e5769af18b93b8
Kyle <hikyle@users.noreply.github.com>
2020-05-08 03:59:10 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ client.on('guildMemberAdd', member => {
         return;
     }
     
-    //channel.send('welcome @' + member.user.username + '#' + member.user.discriminator + ' https://tenor.com/view/forrest-gump-forrest-gump-wave-hello-gif-5576725');
     channel.send('welcome <@' + member.user.id + '> ' + 'https://tenor.com/view/forrest-gump-forrest-gump-wave-hello-gif-5576725');
 });
 
@@ -25,7 +24,7 @@ client.on('guildMemberRemove', member => {
         return;
     }
 
-    //channel.send(member.user.username + ' ' + 'has left https://tenor.com/view/peace-gif-9727828');
+    channel.send(member.user.username + ' ' + 'has left https://tenor.com/view/peace-gif-9727828');
 });
 
 client.login(process.env.BOT_TOKEN);
